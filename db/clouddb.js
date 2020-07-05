af35d12580ef23bae0f46a6e18fe2702b840a860
DeveshChau <deveshachaudhari@gmail.com>
2020-07-05 10:50:37 +0530

    update dbfile

--- db/clouddb.js
+++ db/clouddb.js
@@ -2,12 +2,11 @@
  * Setup the Database URL
  */
 
- // mongodb+srv://admin:<password>@cluster0-46e5h.mongodb.net/test?retryWrites=true&w=majority
 
-const DB_USER = "admin"
-const DB_PASSWORD = "admin123"
+const DB_USER = "devesh"
+const DB_PASSWORD = "7XQ9yzx8Pudn6zpv"
 const DB_NAME = "acmetravel"
-const CLUSTER_HOST = "cluster0-46e5h.mongodb.net"
+const CLUSTER_HOST = "cluster0-1kync.mongodb.net"
 
 // Setup the DB URI
 exports.DB_URI= "mongodb+srv://"+DB_USER+":"+DB_PASSWORD+"@"+CLUSTER_HOST+"/"+DB_NAME+"?retryWrites=true&w=majority"
